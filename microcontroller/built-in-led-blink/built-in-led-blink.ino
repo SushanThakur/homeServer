50ad46fc57ae8f82b74e5af47d7d51929338879c
Sushant Thakur <mail@sushant.name.np>
2024-09-11 23:31:05 +0545

    add microcontroller dir

---
+++ microcontroller/built-in-led-blink/built-in-led-blink.ino
@@ -0,0 +1,58 @@
+#include <ESP8266WiFi.h>
+#include "private.cpp"
+
+#define ledpin 2
+
+WiFiServer server(80);
+String header;
+
+void setup() {
+  Serial.begin(115200);
+
+  WiFi.begin(ssid, pass);
+  Serial.println();
+  Serial.print("connecting");
+  while(WiFi.status() != WL_CONNECTED){
+    Serial.print(".");
+  }
+  Serial.println("Connected Sucessfully.");
+  Serial.println(WiFi.localIP());
+
+  pinMode(ledpin, OUTPUT);
+
+  server.begin();
+
+}
+
+void loop() {
+  delay(500);
+  WiFiClient client = server.available();
+
+  int num;
+
+  if(client){
+    String line="";
+    int i = 0;
+    while(client.connected()){
+      if(client.available()){
+        char c = client.read();
+        line += c;
+        if(i==7){
+          num = (int) (c - '0');
+        }
+        if(c == '\n'){
+          break;
+        }
+        i++;
+      }
+    }
+    //Serial.println(line);
+    Serial.println(num);
+    if(num == 0) {
+      pinMode(ledpin, LOW);
+    }
+    if(num == 1){
+      pinMode(ledpin, HIGH);
+    }
+  }
+}
---
+++ microcontroller/built-in-led-blink/private.cpp
@@ -0,0 +1,2 @@
+const char* ssid = "Durdarsi (Far Sighted)_2.4";
+const char* pass = "Whatdahellisthes";
